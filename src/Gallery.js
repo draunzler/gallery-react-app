2d3281d8b67284bf653a340a06d829e81270716f
Draunzler <shadymeister2017@gmail.com>
2023-05-31 01:47:22 +0530

    commit

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 import img1 from './img/img1.jpg';
-import img2 from './img/img1.jpg';
-import img3 from './img/img1.jpg';
-import img4 from './img/img1.jpg';
-import img5 from './img/img1.jpg';
-import img6 from './img/img1.jpg';
-import img7 from './img/img1.jpg';
-import img8 from './img/img1.jpg';
-import img9 from './img/img1.jpg';
-import img10 from './img/img1.jpg';
-import img11 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/img6.jpg';
+import img7 from './img/img7.jpg';
+import img8 from './img/img8.jpg';
+import img9 from './img/img9.png';
+import img10 from './img/img10.jpg';
+import img11 from './img/img11.jpg';
 
 const Gallery = () => {
     
@@ -67,7 +67,7 @@ const Gallery = () => {
                 data.map((item, index)=>{
                     return(
                         <div className="pics" key={index}>
-                            <img src={item.imgSrc} />
+                            <img src={item.imgSrc} style={{width: '100%'}}/>
                         </div>
                     )
                 })
